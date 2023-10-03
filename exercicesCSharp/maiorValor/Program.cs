a3004eef71c7c832e56a3fc556c0cf795c29150e
Isac Vieira <isacvieira2009@hotmail.com>
2023-10-02 22:16:40 -0300

    exercism CSharp

--- exercicesCSharp/maiorValor/Program.cs
+++ exercicesCSharp/maiorValor/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static int MaiorElemento(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            // -Infinity serve para sinalizar o menor número possível,
+            // visto que se fosse zero estaria errado pois assumiriamos que o 0 é o maior número.
+            return int.MinValue;
+        }
+        else
+        {
+            int primeiroElemento = arr[0];
+            int[] restanteElementos = arr.Skip(1).ToArray();
+            int maiorElementoEncontrado = MaiorElemento(restanteElementos);
+            
+            // Leia "retorne o primeiroElemento caso ele seja maior que o maiorElementoEncontrado,
+            // caso não seja defina o primeiroElemento com o valor de maiorElementoEncontrado"
+            return primeiroElemento > maiorElementoEncontrado
+                ? primeiroElemento
+                : maiorElementoEncontrado;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] teste = { 1, 2, 4, 10 };
+        Console.WriteLine(MaiorElemento(teste));
+        Console.ReadKey();
+    }
+}
